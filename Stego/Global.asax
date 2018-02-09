8b85501c73e2ee0ede296d8d941c45d953c1c303
Christian Beaulieu <cgbeaulieu@gmail.com>
2018-02-08 21:27:30 -0700

    Initial Commit

---
+++ Stego/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stego.MvcApplication" Language="C#" %>
---
+++ Stego/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stego.Startup))]
+namespace Stego
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
